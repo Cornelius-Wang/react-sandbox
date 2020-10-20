43b61f1a5c1322a62414ff90c723aa25f4dfbc53
Cory Wang <cory.wang11@gmail.com>
2020-10-20 12:24:16 -0700

    new exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip';
+
+const firstToolTip = (
+  <Tooltip message='tooltip message' color='hotpink'>
+    Lorem
+  </Tooltip>
+);
+
+const secondToolTip = (
+  <Tooltip message='another tooltip message' color='#126BCC'>
+    Ipsum
+  </Tooltip>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      {/* content goes here */}
+      <Split className='left' flexBasis='2'>
+        This is the content for the left `Split`. Lorem {firstToolTip} ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content for the right `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit.  {secondToolTip}  Incidunt ex velit suscipit facere officia?
+      </Split>
+    </main>
   );
 }
 
