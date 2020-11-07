d06f2cb7f690bb2d5b0a35f15f811633e6d2581c
Cory Wang <cory.wang11@gmail.com>
2020-11-06 17:06:07 -0800

    updated state drill apps

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import Split from './composition/Split';
+// import Split from './composition/Split';
 import './App.css';
-import Tooltip from './composition/Tooltip';
+// import Tooltip from './composition/Tooltip';
+// import TheDate from './state-drills/TheDate';
+import Counter from './state-drills/Counter';
 
-const firstToolTip = (
-  <Tooltip message='tooltip message' color='hotpink'>
-    Lorem
-  </Tooltip>
-);
-
-const secondToolTip = (
-  <Tooltip message='another tooltip message' color='#126BCC'>
-    Ipsum
-  </Tooltip>
-)
 
 function App() {
   return (
     <main className='App'>
       {/* content goes here */}
-      <Split className='left' flexBasis='2'>
-        This is the content for the left `Split`. Lorem {firstToolTip} ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit.  {secondToolTip}  Incidunt ex velit suscipit facere officia?
-      </Split>
+      <Counter />
     </main>
   );
 }
